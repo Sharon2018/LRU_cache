4b41abae0c91dd873ce0d924e68e7b9c8c34197c
sfban <36094408+sfban@users.noreply.github.com>
2018-08-03 15:35:26 -0400

    Add files via upload

---
+++ source_code/cache_LRU.java
@@ -0,0 +1,74 @@
+class Node{
+    int key;
+    int value;
+    Node pre;
+    Node next; 
+    public Node(int key, int value){
+        this.key = key;
+        this.value = value;
+    }
+}
+
+public class LRUCache {
+    int capacity;
+    Map<Integer, Node> map = new HashMap<>();
+    Node head=null;
+    Node end=null;
+ 
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+ 
+    public void remove(Node n){
+        if(n.pre!=null){
+            n.pre.next = n.next;
+        }else{
+            head = n.next;
+        }
+        if(n.next!=null){
+            n.next.pre = n.pre;
+        }else{
+            end = n.pre;
+        } 
+    }
+ 
+    public void setHead(Node n){
+        n.next = head;
+        n.pre = null;
+        if(head!=null){
+            head.pre = n;
+        }
+        head = n;
+        if(end ==null) {
+            end = head;
+        }
+    }
+
+    public int get(int key) {
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            remove(node);
+            setHead(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+ 
+    public void set(int key, int value) {
+        if(map.containsKey(key)){
+            Node old = map.get(key);
+            old.value = value;
+            remove(old);
+            setHead(old);
+        }else{
+            Node created = new Node(key, value);
+            if(map.size()>=capacity){
+                map.remove(end.key);
+                remove(end);
+            }
+            setHead(created);    
+            map.put(key, created);
+        }
+    }
+}
